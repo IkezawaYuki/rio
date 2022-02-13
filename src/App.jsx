f2344cc05f33ac8458b7f35e1e5868b3c719b6bc
yikezawa <y.ikezawa93@gmail.com>
2022-02-13 19:51:20 +0900

    mod

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,26 @@
-import { useEffect, useState, useReducer } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
-import React from 'react';
-import { Nav } from "./components/Nav";
-import { Row } from './components/Row';
-import { Banner } from './components/Banner';
-import { requests } from "./requests.js";
-import EventForm from "./components/EventForm";
-import reducer from "./reducers/index";
-import EventIndex from './components/EventIndex';
-import AppContext from "./contexts/AppContext";
-import OperationLogs from "./components/OperationLogs";
+import React from "react";
 
+const getMedia = async () => {
+  const constraints = {
+    audio: true,
+    video: true
+  };
+  let stream = null;
 
-const App = () => {
-  const initialtState = {
-    events: [],
-    operationLogs: []
+  try {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+  } catch {
+
   }
-  const [state, dispatch] = useReducer(reducer, initialtState);
-  return (
-    <AppContext.Provider value={{ state, dispatch }}>
-      <div className='container-fluid'>
-        <EventForm />
-        <EventIndex />
-        <OperationLogs />
-      </div>
-    </AppContext.Provider>
-  )
 }
 
-App.defaultProps = {
-  name: "サンプル",
-  price: 1000
+const App = () => {
+  getMedia();
+  return (
+    <>
+      <div>Hello World</div>
+    </>
+  )
 }
 
 export default App
